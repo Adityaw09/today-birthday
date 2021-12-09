dfda5807957c44a2332d9073cbd18cbff5d2392f
Adityaw09 <adityaw097@gmail.com>
2021-12-09 17:53:05 +0530

    todays-birthday

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id:1,
+        name:"Santosh",
+        age: 45,
+        image: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i3Yp1mXpDIMA/v2/-1x-1.jpg"
+    },
+    {
+        id:2,
+        name:"Suyog",
+        age: 45,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2WGH0vbM87BdENSW0QmyfbIlaaMnmqiRYRA&usqp=CAU"
+    },
+    {
+        id:3,
+        name:"Sushant",
+        age: 45,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Rp476CaKmnBQjHZbfpFAoEs4kY2FfqjLBA&usqp=CAU"
+    },
+    {
+        id:4,
+        name:"Chinmay",
+        age: 45,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfqQCat6Zly-xTcvEl8JCdZ_KfbbEkUGIQzA&usqp=CAU"
+    },
+    {
+        id:5,
+        name:"Mayur",
+        age: 45,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvbSDPpMpJNB1fyKpCuWy8zaWT56h-4y1HKQ&usqp=CAU"
+    }
+]
